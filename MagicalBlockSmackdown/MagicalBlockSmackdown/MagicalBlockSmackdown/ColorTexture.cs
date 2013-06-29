528e4bd3eaed95b9aeafb87beecce0ef2ecde6a5
Daniel Savage <dansvg@gmail.com>
2013-06-29 11:31:27 -0700

    initial project code

---
+++ MagicalBlockSmackdown/MagicalBlockSmackdown/MagicalBlockSmackdown/ColorTexture.cs
@@ -0,0 +1,58 @@
+﻿namespace Utilities
+{
+    using System;
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    /// <summary>
+    /// Creates single color textures (i.e. for drawing rectangles).
+    /// </summary>
+    class ColorTexture
+    {
+        /// <summary>
+        /// Creates a 1x1 pixel black texture.
+        /// </summary>
+        /// <param name="graphicsDevice">The graphics device to use.</param>
+        /// <returns>The newly created texture.</returns>
+        public static Texture2D Create(GraphicsDevice graphicsDevice)
+        {
+            return Create(graphicsDevice, 1, 1, new Color());
+        }
+
+        /// <summary>
+        /// Creates a 1x1 pixel texture of the specified color.
+        /// </summary>
+        /// <param name="graphicsDevice">The graphics device to use.</param>
+        /// <param name="color">The color to set the texture to.</param>
+        /// <returns>The newly created texture.</returns>
+        public static Texture2D Create(GraphicsDevice graphicsDevice, Color color)
+        {
+            return Create(graphicsDevice, 1, 1, color);
+        }
+
+        /// <summary>
+        /// Creates a texture of the specified color.
+        /// </summary>
+        /// <param name="graphicsDevice">The graphics device to use.</param>
+        /// <param name="width">The width of the texture.</param>
+        /// <param name="height">The height of the texture.</param>
+        /// <param name="color">The color to set the texture to.</param>
+        /// <returns>The newly created texture.</returns>
+        public static Texture2D Create(GraphicsDevice graphicsDevice, int width, int height, Color color)
+        {
+            Texture2D texture = new Texture2D(graphicsDevice, width, height, false, SurfaceFormat.Color);
+
+            // Create a color array for the pixels
+            Color[] colors = new Color[width * height];
+            for (int i = 0; i < colors.Length; i++)
+            {
+                colors[i] = new Color(color.ToVector3());
+            }
+
+            // Set the color data for the texture
+            texture.SetData(colors);
+
+            return texture;
+        }
+    }
+}
---
+++ MagicalBlockSmackdown/MagicalBlockSmackdown/MagicalBlockSmackdown/GameplayModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MagicalBlockSmackdown
+{
+    class GameplayModel
+    {
+        public enum PanelColor
+        {
+            Red = 0,
+            Blue = 1,
+            Green = 2,
+            Yellow = 3,
+            Cyan = 4,
+            Purple = 5,
+        }
+
+        public enum PanelState
+        {
+            None = 0,
+            Alive = 1,
+            Falling = 2,
+            Exploding = 2,
+        }
+
+        public struct Panel
+        {
+            public PanelColor color;
+            public PanelState state;
+
+            public Panel(PanelColor color, PanelState state)
+            {
+                this.color = color;
+                this.state = state;
+            }
+
+            public void activate(PanelColor color)
+            {
+                state = PanelState.Alive;
+                this.color = color;
+            }
+
+            public Color PanelColorValue()
+            {
+                switch (color)
+                {
+                    case PanelColor.Red:
+                        return Color.Red;
+                    case PanelColor.Green:
+                        return Color.Green;
+                    case PanelColor.Blue:
+                        return Color.Blue;
+                    case PanelColor.Cyan:
+                        return Color.Cyan;
+                    case PanelColor.Purple:
+                        return Color.Purple;
+                    case PanelColor.Yellow:
+                        return Color.Yellow;
+                    default:
+                        return Color.White;
+                }
+            }
+        }
+
+        private const int gridWidth = 6;
+        private const int gridHeight = 12;
+        private Panel[,] grid;
+        public Panel[,] Grid { get { return grid; } }
+
+        public GameplayModel()
+        {
+            grid = new Panel[gridWidth, gridHeight];
+        }
+
+        public void update(GameTime currentTime)
+        {
+            return;
+        }
+    }
+}
